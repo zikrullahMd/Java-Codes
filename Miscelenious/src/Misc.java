375b953def6f19ec8437d586f1d1515e10423934
Md Zikrullah <mohmmedzikrullah159@gmail.com>
2021-05-20 12:09:51 +0530

    adding files

--- Miscelenious/src/Misc.java
+++ Miscelenious/src/Misc.java
@@ -3,38 +3,61 @@ import java.math.*;
 import java.util.Map.Entry;
 class Misc{
 	
+	public static int PostFix(String s) {
+		Stack<Integer> st = new Stack<>();
+        int res = 0;
+        for(int i = 0;i<s.length();i++) {
+        	char c = s.charAt(i);
+        	if(Character.isDigit(c)) {
+        		st.push(c-'0');
+        	}
+        	else {
+        		int one = st.pop();
+        		int two = st.pop();
+        		switch(c) {
+        		case '+' : st.push((one+two));
+        		break;
+        		case '-' : st.push((one - two));
+        		break;
+        		case '*' : st.push((one*two));
+        		break;
+        		case '/' : st.push((Math.max(one, two) / Math.min(one, two)));
+        		break;
+        		}
+        	}
+        }
+        return st.peek();
+	}
+	
+	public static int PreFix(String s) {
+		Stack<Integer> st = new Stack<>();
+        int res = 0;
+        for(int i = s.length()-1;i>=0;i--) {
+        	char c = s.charAt(i);
+        	if(Character.isDigit(c)) {
+        		st.push(c-'0');
+        	}
+        	else {
+        		int one = st.pop();
+        		int two = st.pop();
+        		switch(c) {
+        		case '+' : st.push((one+two));
+        		break;
+        		case '-' : st.push((one - two));
+        		break;
+        		case '*' : st.push((one*two));
+        		break;
+        		case '/' : st.push((Math.max(one, two) / Math.min(one, two)));
+        		break;
+        		}
+        	}
+        }
+        return st.peek();
+	}
+	
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int[][] a = new int[n+1][n+1];
-        int left = 0;
-        int top = 0;
-        int right = a[0].length-1;
-        int bottom = a.length-1;
-        int num = 1;
-        while(num <= n*n){
-            for(int i = left;i<=right;i++){
-                a[top][i] = num++;
-            }
-            top++;
-            for(int i = top;i<=bottom;i++){
-                a[i][right] = num++;
-            }
-            right--;
-            for(int i = right;i>=left;i--){
-                a[bottom][i] = num++;
-            }
-            bottom--;
-            for(int i = bottom;i>=top;i--){
-                a[i][left] = num++;
-            }
-            left++;
-        }
-        for(int i = 0;i<a.length;i++) {
-        	for(int j = 0;j<a[0].length;j++) {
-        		System.out.print(a[i][j]+" ");
-        	}
-        	System.out.println();
-        }
+        String s = in.next();
+        System.out.println(PreFix(s));
     }
 }
