5452d2a71a59ff05ab373e7258c9b76b3829ede2
Md Zikrullah <mohmmedzikrullah159@gmail.com>
2021-05-20 23:30:13 +0530

    solved cses problems

--- Miscelenious/src/Misc.java
+++ Miscelenious/src/Misc.java
@@ -1,63 +1,75 @@
 import java.util.*;
 import java.math.*;
 import java.util.Map.Entry;
-class Misc{
-	
-	public static int PostFix(String s) {
-		Stack<Integer> st = new Stack<>();
-        int res = 0;
-        for(int i = 0;i<s.length();i++) {
-        	char c = s.charAt(i);
-        	if(Character.isDigit(c)) {
-        		st.push(c-'0');
-        	}
-        	else {
-        		int one = st.pop();
-        		int two = st.pop();
-        		switch(c) {
-        		case '+' : st.push((one+two));
-        		break;
-        		case '-' : st.push((one - two));
-        		break;
-        		case '*' : st.push((one*two));
-        		break;
-        		case '/' : st.push((Math.max(one, two) / Math.min(one, two)));
-        		break;
-        		}
-        	}
-        }
-        return st.peek();
-	}
-	
-	public static int PreFix(String s) {
-		Stack<Integer> st = new Stack<>();
-        int res = 0;
-        for(int i = s.length()-1;i>=0;i--) {
-        	char c = s.charAt(i);
-        	if(Character.isDigit(c)) {
-        		st.push(c-'0');
-        	}
-        	else {
-        		int one = st.pop();
-        		int two = st.pop();
-        		switch(c) {
-        		case '+' : st.push((one+two));
-        		break;
-        		case '-' : st.push((one - two));
-        		break;
-        		case '*' : st.push((one*two));
-        		break;
-        		case '/' : st.push((Math.max(one, two) / Math.min(one, two)));
-        		break;
-        		}
-        	}
-        }
-        return st.peek();
-	}
-	
-    public static void main(String[] args){
+public class Misc
+{
+    public static void main(String[] args)
+    {
         Scanner in = new Scanner(System.in);
         String s = in.next();
-        System.out.println(PreFix(s));
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        System.out.println(s.substring(start,end+1));
+    }
+    public static int expandAroundCenter(String s, int left , int right) {
+    	int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
     }
 }
+
+
+class Node{
+	LinkedList[] adj;
+	int v;
+	public Node(int v) {
+		this.v = v;
+		adj = new LinkedList[v];
+		for(int i = 0;i<v;i++) {
+			adj[i] = new LinkedList<Integer>();
+		}
+	}
+	
+	public void addEdge(int s, int d) {
+		adj[s].add(d);
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
