50c60d8ed9a4e0afe82fb66af61246e349f70462
Md Zikrullah <mohmmedzikrullah159@gmail.com>
2021-05-21 21:46:46 +0530

    todays practice

--- Miscelenious/src/Misc.java
+++ Miscelenious/src/Misc.java
@@ -6,55 +6,72 @@ public class Misc
     public static void main(String[] args)
     {
         Scanner in = new Scanner(System.in);
-        String s = in.next();
-        int start = 0;
-        int end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = Math.max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+        int n = in.nextInt();
+        ListNode l = new ListNode(0);
+        ListNode curr = l;
+        for(int i = 0;i<n;i++) {
+        	curr.next = new ListNode(in.nextInt());
+        	curr = curr.next;
+        	System.out.println("for");
+        }
+        Solution s = new Solution();
+        ListNode ans = s.solve(l.next);
+        ListNode head = ans;
+        while(ans != null) {
+        	System.out.print(ans.val+" ");
+        	ans = ans.next;
+        }
+        
+}
+}
+class ListNode {
+    public int val;
+    public ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+ 
+class Solution {
+    public ListNode solve(ListNode root) {
+        if(root == null) return root;
+        int n = 0;
+        ListNode curr = root;
+        while(curr != null){
+            n++;
+            curr = curr.next;
+        }
+        if(n <= 3){
+            return root;
+        }
+        curr = root;
+        int temp = 1;
+        Stack<ListNode> st = new Stack<>();
+        Queue<ListNode> q = new LinkedList<>();
+        while(curr != null){
+            if(temp % 2 == 0){
+                st.push(curr);
+            }else{
+                q.add(curr);
             }
+            temp++;
+            curr = curr.next;
         }
-        System.out.println(s.substring(start,end+1));
-    }
-    public static int expandAroundCenter(String s, int left , int right) {
-    	int L = left, R = right;
-        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
-            L--;
-            R++;
+        temp = 1;
+        ListNode ans = new ListNode(0);
+        ListNode head = ans;
+        while(!st.isEmpty() || !q.isEmpty()){
+            if(temp % 2 != 0 && !q.isEmpty()){
+                head.next = q.poll();
+                head = head.next;
+            }else if(temp % 2 == 0 && !st.isEmpty()){
+                head.next = st.pop();
+                head = head.next;
+            }
+            temp++;
         }
-        return R - L - 1;
+        return ans.next;
     }
+
 }
-
-
-class Node{
-	LinkedList[] adj;
-	int v;
-	public Node(int v) {
-		this.v = v;
-		adj = new LinkedList[v];
-		for(int i = 0;i<v;i++) {
-			adj[i] = new LinkedList<Integer>();
-		}
-	}
-	
-	public void addEdge(int s, int d) {
-		adj[s].add(d);
-	}
-	
-}
-
-
-
-
-
-
-
-
 
 
 
